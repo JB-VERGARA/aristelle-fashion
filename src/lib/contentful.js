191bf8b753f6ae9b65bd08914fd1c30aa0b56163
JB-VERGARA <vergarajohnbryan.dev@gmail.com>
2024-11-25 23:06:59 +0800

    Fixing Deployment issues

--- src/lib/contentful.js
+++ src/lib/contentful.js
@@ -1,6 +1,10 @@
 // contentful.js
 import { createClient } from 'contentful';
 
+if (!process.env.CONTENTFUL_SPACE_ID || !process.env.CONTENTFUL_ACCESS_TOKEN) {
+  throw new Error("Missing Contentful environment variables");
+}
+
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
